a40a4967ae2a4a50c4ca55ee8baf045a7ec3ed27
Eric <ericgeorge4477@gmail.com>
2023-08-17 13:30:45 -0700

    3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -28,25 +28,25 @@ int convert_day(int month, int day)
             day = 151 + day;
             break;
         case 7:
-            day = 181 + day;
-            break;
+	day = 181 + day;
+	break;
         case 8:
-            day = 212 + day;
-            break;
-        case 9:
-            day = 243 + day;
-            break;
-        case 10:
-            day = 273 + day;
-            break;
-        case 11:
-            day = 304 + day;
-            break;
-        case 12:
-            day = 334 + day;
-            break;
-        default:
-            break;
-    }
-    return (day);
+	day = 212 + day;
+	break;
+	case 9:
+	day = 243 + day;
+	break;
+	case 10:
+	day = 273 + day;
+	break;
+	case 11:
+	day = 304 + day;
+	break;
+	case 12:
+	day = 334 + day;
+	break;
+	default:
+	break;
+	}
+	return (day);
 }
